9650fcd8f86a5b131bc91aa8e1ee1abb3a869c09
devgithubsharma <devsharmabsr03@gmail.com>
2023-06-08 00:04:48 +0530

    changes

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const baseURL =
     process.env.NODE_ENV === "production"
-        ? "https://backend-typometer.herokuapp.com/api"
+        ? "https://typometers-backends.onrender.com"
         : "http://localhost:4000";
 
 const client = axios.create({ baseURL });
