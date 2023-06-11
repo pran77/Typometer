14a5120956abed772b0289747797899a4c64ecb8
devgithubsharma <devsharmabsr03@gmail.com>
2023-06-11 20:50:46 +0530

    changes

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-const Base_URL =
-    process.env.NODE_ENV === "production"
-        ? "https://typometer-backend.onrender.com"
-        : "http://localhost:4000";
+// const Base_URL =
+//     process.env.NODE_ENV === "production"
+//         ? "https://typometer-backend.onrender.com"
+//         : "http://localhost:4000";
 
-const client = axios.create({ Base_URL });
+const BASE_URL = "https://typometer-backend.onrender.com";
+const client = axios.create({ BASE_URL });
 export default client;
 
